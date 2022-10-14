6b57393cb28e91e9ba71e9c4e37c5ab4c8cceeb3
edwinrsalazara <edwin.salazar1.mt@usa.edu.co>
2022-10-13 21:16:27 -0500

    first commit

---
+++ src/main/java/com/usa/palcosapp/model/Reservation.java
@@ -0,0 +1,35 @@
+package com.usa.palcosapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "reservation")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idReservation;
+    private Date starDate;
+    private Date devolutionDate;
+    private String status = "created";
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    @JsonIgnoreProperties("reservations")
+    private Box box;
+
+    @ManyToOne
+    @JoinColumn(name = "idClient")
+    @JsonIgnoreProperties({"reservations","message"})
+    private Client client;
+    private String score;
+}
